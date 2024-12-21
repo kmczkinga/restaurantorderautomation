3419b7ff6cdc35c2cb16279be642d25dcc90a64a
kmczkinga <146724927+kmczkinga@users.noreply.github.com>
2024-12-21 11:52:31 +0100

    Update RestaurantOrderAutomationApplication.java

--- src/main/java/hu/nye/it/RestaurantOrderAutomation/RestaurantOrderAutomationApplication.java
+++ src/main/java/hu/nye/it/RestaurantOrderAutomation/RestaurantOrderAutomationApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestaurantOrderAutomationApplication {
 
+       /**
+        * Main függvény.
+        * Elindítás után lefuttatja a /resources/schema.sql és /resources/data.sql fájlokat
+        */
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantOrderAutomationApplication.class, args);
 	}
